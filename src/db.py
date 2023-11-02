04bec36d917148cc7cbbe38d25c3647b98ca2ae1
SergiyBagmet <134723713+SergiyBagmet@users.noreply.github.com>
2023-11-02 18:40:18 +0200

    configparser

---
+++ src/db.py
@@ -0,0 +1,21 @@
+import configparser
+from pathlib import Path
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+file_config = Path(__file__).parent.parent.joinpath("config.ini")
+config = configparser.ConfigParser()
+config.read(file_config)
+
+username = config.get("DB", "USER")
+password = config.get("DB", "PASSWORD")
+db_name = config.get("DB", "DB_NAME")
+domain = config.get("DB", "DOMEIN")
+port = config.get("DB", "PORT")
+url = f"postgresql://{username}:{password}@{domain}:{port}/{db_name}"
+
+engine = create_engine(url, echo=True)
+DBsession = sessionmaker(bind=engine)
+session = DBsession()
