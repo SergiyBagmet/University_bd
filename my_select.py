f52518ef9ecf86510ca9fff6a77a5238367da429
SergiyBagmet <134723713+SergiyBagmet@users.noreply.github.com>
2023-11-03 21:56:16 +0200

    select 6-10

--- my_select.py
+++ my_select.py
@@ -89,13 +89,94 @@ class Selecter:
             .where(Teacher.id == t_id)
         )
         return query
+    
+    def select_6(self, gr_id=1) -> Query :
+        self.titel = "6. Знайти список студентів у певній групі."
+        query = (
+            select(
+                func.concat(Student.first_name, ' ', Student.last_name).label('students'),
+                func.max(Group.name).label('group'),
+            )
+            .select_from(Student)
+            .join(Group, Student.group_id == Group.id)
+            .where(Group.id == gr_id)
+            .group_by('students')
+        )
+        return query
+    
+    def select_7(self, gr_id=1, sub_id=1) -> Query :
+        self.titel = "7. Знайти оцінки студентів у окремій групі з певного предмета."
+        query = (
+            select(
+                Grade.score.label('grades'),
+                func.concat(Student.first_name, ' ', Student.last_name).label('students'),
+                func.max(Group.name).label('group'),
+                func.max(Subject.name).label('subject')
+            )
+            .select_from(Grade)
+            .join(Student, Grade.student_id == Student.id)
+            .join(Subject, Grade.subject_id == Subject.id)
+            .join(Group, Student.group_id == Group.id)
+            .filter(and_(Group.id == gr_id, Subject.id == sub_id))
+            .group_by('grades', 'students')
+        )
+        return query
+    
+    def select_8(self, t_id=4) -> Query :
+        self.titel = "8. Знайти середній бал, який ставить певний викладач зі своїх предметів."
+        query = (
+            select(
+                Subject.name.label('subjects'),
+                func.concat(Teacher.first_name, ' ', Teacher.last_name).label('teacher'),
+                func.round(func.avg(Grade.score), 2).label('average_grade'),   
+            )
+            .select_from(Grade)
+            .join(Subject, Grade.subject_id == Subject.id)
+            .join(Teacher, Teacher.id == Subject.teacher_id)
+            .where(Teacher.id == t_id)
+            .group_by('subjects', 'teacher')
+            .order_by(desc('average_grade'))
+        )
+        return query
+
+    def select_9(self, student_id=3):
+        self.titel = "9. Знайти список курсів, які відвідує студент."
+        query = (
+            select(
+                Subject.name.label('subjects'),
+                func.concat(Student.first_name, ' ', Student.last_name).label('student')
+            )
+            .select_from(Grade)
+            .join(Student, Grade.student_id == Student.id)
+            .join(Subject, Grade.subject_id == Subject.id)
+            .where(Student.id == student_id)
+            .group_by('subjects', 'student')
+        )
+        return query
+    
+    def select_10(self, student_id=1, teacher_id=4):
+        self.titel = "10. Список курсів, які певному студенту читає певний викладач."
+        query = (
+            select(
+                Subject.name.label('subjects'),
+                func.concat(Student.first_name, ' ', Student.last_name).label('student'),
+                func.concat(Teacher.first_name, ' ', Teacher.last_name).label('teacher')
+            )
+            .select_from(Grade)
+            .join(Student, Grade.student_id == Student.id)
+            .join(Subject, Grade.subject_id == Subject.id)
+            .join(Teacher, Teacher.id == Subject.teacher_id)
+            .where(and_(Student.id == student_id, Teacher.id == teacher_id))
+            .group_by('subjects', 'student', 'teacher')
+        )
+        return query
 
 if __name__ == "__main__":
     dbm = DBManager(url)
     crud_m = CRUDManager(dbm)
     sel = Selecter()
 
-    for i in range(5, 6):
+    for i in range(10, 11):
         query : Query = getattr(sel, f'select_{i}')()
         titel = sel.titel
         print(query.column_descriptions)
